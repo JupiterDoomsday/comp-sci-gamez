f5a181e96ac42d2648da72c44607103c45419394
Alexis Tinoco <atinoco25@email.arizona.edu>
2019-10-03 10:09:27 -0700

    First push for java quizlet

---
+++ src/main/java/model/JavaQuizlet.java
@@ -0,0 +1,10 @@
+package model;
+
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class JavaQuizlet extends VBox{
+	private Label welcomeLabel = new Label("Welcome to Java Quizlet!");
+	
+	
+}
