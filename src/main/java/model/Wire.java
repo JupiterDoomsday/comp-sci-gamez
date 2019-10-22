8352c94042622a56596949af90ec50dd2e750b05
cssetton <cssetton@email.arizona.edu>
2019-10-21 20:10:37 -0700

    made the tiles

--- src/main/java/model/Wire.java
+++ src/main/java/model/Wire.java
@@ -1,7 +1,12 @@
 package model;
 
+import com.sun.glass.ui.Application.EventHandler;
+
+import javafx.scene.control.Button;
+
 public class Wire {
 	public boolean state;
+	public Button button;
 	 public Wire(boolean b) {
 		 this.state=b;
 	 }
