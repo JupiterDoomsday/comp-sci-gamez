83883c94d985445a9dcc10edbadf831f4edbdb60
tylerlancast <tylerlancast@hotmail.com>
2019-11-04 23:12:49 -0700

    Finished tutorial layout

--- src/main/java/view/BinaryBattleView.java
+++ src/main/java/view/BinaryBattleView.java
@@ -24,8 +24,10 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 public class BinaryBattleView extends VBox{
 	
@@ -33,6 +35,7 @@ public class BinaryBattleView extends VBox{
 	private ArrayList<String> binaryStrings;
 	private ArrayList<Text> textList;
 	private int childToChange = 0;
+	private VBox vBox;
 	
 	public BinaryBattleView() {
 		
@@ -85,7 +88,7 @@ public class BinaryBattleView extends VBox{
 		
 		this.setMinWidth(1200);
 		this.setMinHeight(900);
-		VBox vBox = new VBox();
+		vBox = new VBox();
 		//this.setBackground(new Background(new BackgroundFill(Color.BLACK, null, null)) );
 		Text welcomeMessage = new Text("Welcome to Binary Battle!");
 		Text selectLevelMessage = new Text("Select the level you wish to play.");
@@ -110,6 +113,16 @@ public class BinaryBattleView extends VBox{
 		tutorialButton.setOnMouseExited(e -> {
 			tutorialButton.setStyle(buttonStyle);
 		});
+		
+		tutorialButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent ae) {
+				// Clear background and current vBox
+				vBox.getChildren().clear();
+				layoutTutorial();
+			}
+		});
+		
 		Button nimbleNibbles = new Button("Nimble Nibbles");
 		nimbleNibbles.setMinHeight(150);
 		nimbleNibbles.setMinWidth(500);
@@ -121,6 +134,16 @@ public class BinaryBattleView extends VBox{
 		nimbleNibbles.setOnMouseExited(e -> {
 			nimbleNibbles.setStyle(buttonStyle);
 		});
+		
+		nimbleNibbles.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent ae) {
+				// Clear background and current vBox
+				vBox.getChildren().clear();
+				layoutNimbleNibbles();
+			}
+		});
+		
 		Button bulletBytes = new Button("Bullet Bytes");
 		bulletBytes.setMinHeight(150);
 		bulletBytes.setMinWidth(500);
@@ -147,53 +170,8 @@ public class BinaryBattleView extends VBox{
 		Text startMessage = new Text("Enter the base-10 values of the following numbers: ");
 		startMessage.setFont(Font.font("Sans-serif", FontWeight.BOLD, 28));
 		startMessage.setFill(Color.WHITE);
-		//this.getChildren().add(startMessage);
-		
-		/*
-		Text lineBreak = new Text("");
-		Text num0 = new Text("   " + binaryStrings.get(0));
-		Text num1 = new Text("   " + binaryStrings.get(1));
-		Text num2 = new Text("   " + binaryStrings.get(2));
-		Text num3 = new Text("   " + binaryStrings.get(3));
-		Text num4 = new Text("   " + binaryStrings.get(4));
-		Text num5 = new Text("   " + binaryStrings.get(5));
-		Text num6 = new Text("   " + binaryStrings.get(6));
-		Text num7 = new Text("   " + binaryStrings.get(7));
-		Text num8 = new Text("   " + binaryStrings.get(8));
-		Text num9 = new Text("   " + binaryStrings.get(9));
-		num0.setFont(Font.font("Sans-serif", 20));
-		num0.setFill(Color.DARKGREEN);
-		num0.setId("#b0");
-		num1.setFont(Font.font("Sans-serif", 20));
-		num1.setFill(Color.DARKGREEN);
-		num2.setFont(Font.font("Sans-serif", 20));
-		num2.setFill(Color.DARKGREEN);
-		num3.setFont(Font.font("Sans-serif", 20));
-		num3.setFill(Color.DARKGREEN);
-		num4.setFont(Font.font("Sans-serif", 20));
-		num4.setFill(Color.DARKGREEN);
-		num5.setFont(Font.font("Sans-serif", 20));
-		num5.setFill(Color.DARKGREEN);
-		num6.setFont(Font.font("Sans-serif", 20));
-		num6.setFill(Color.DARKGREEN);
-		num7.setFont(Font.font("Sans-serif", 20));
-		num7.setFill(Color.DARKGREEN);
-		num8.setFont(Font.font("Sans-serif", 20));
-		num8.setFill(Color.DARKGREEN);
-		num9.setFont(Font.font("Sans-serif", 20));
-		num9.setFill(Color.DARKGREEN);
-		this.getChildren().add(num0);
-		this.getChildren().add(num1);
-		this.getChildren().add(num2);
-		this.getChildren().add(num3);
-		this.getChildren().add(num4);
-		this.getChildren().add(num5);
-		this.getChildren().add(num6);
-		this.getChildren().add(num7);
-		this.getChildren().add(num8);
-		this.getChildren().add(num9);
-		*/
-		
+
+		/*		
 		for(int i = 0;i < listOfNumbers.size();i++) {
 			Text newText = new Text("   " + binaryStrings.get(i));
 			//System.out.println("TEST: newText is: " + newText.getText());
@@ -235,14 +213,142 @@ public class BinaryBattleView extends VBox{
 				userGuess.clear();
 			}
 		});
-		
+		*/
 
-		
-		
-		
-		
 	}
 	
+	private void layoutTutorial() {
+		vBox.setAlignment(Pos.TOP_LEFT);
+		
+		TextFlow flow = new TextFlow();
+		
+		Text text1 = new Text("Binary Battle");
+		text1.setFill(Color.LAWNGREEN);
+		text1.setFont(Font.font("Sans-serif", FontWeight.BOLD, FontPosture.ITALIC, 28));
+		Text text2 = new Text(" is all about converting binary numbers to their decimal form.\n\n");
+		text2.setFill(Color.WHITE);
+		text2.setFont(Font.font("Sans-serif", FontWeight.NORMAL, FontPosture.REGULAR, 24));
+		Text text3 = new Text("Before we get started, we need to cover three basic concepts:\n\n");
+		text3.setFill(Color.WHITE);
+		text3.setFont(Font.font("Sans-serif", FontWeight.NORMAL, FontPosture.REGULAR, 24));
+		Text text4 = new Text(" - Binary numbers are made up of individual ");
+		text4.setFill(Color.WHITE);
+		text4.setFont(Font.font("Sans-serif", FontWeight.NORMAL, FontPosture.REGULAR, 24));
+		Text text5 = new Text("bits");
+		text5.setFill(Color.WHITE);
+		text5.setFont(Font.font("Sans-serif", FontWeight.BOLD, FontPosture.ITALIC, 28));
+		text5.setUnderline(true);
+		Text text6 = new Text(" which can be either a 0, or a 1.\n");
+		text6.setFill(Color.WHITE);
+		text6.setFont(Font.font("Sans-serif", FontWeight.NORMAL, FontPosture.REGULAR, 24));
+		Text text7 = new Text(" - A ");
+		text7.setFill(Color.WHITE);
+		text7.setFont(Font.font("Sans-serif", FontWeight.NORMAL, FontPosture.REGULAR, 24));
+		Text text8 = new Text("nibble");
+		text8.setFill(Color.WHITE);
+		text8.setFont(Font.font("Sans-serif", FontWeight.BOLD, FontPosture.ITALIC, 28));
+		text8.setUnderline(true);
+		Text text9 = new Text(" is made up of 4 bits, such as '0001'.\n");
+		text9.setFill(Color.WHITE);
+		text9.setFont(Font.font("Sans-serif", FontWeight.NORMAL, FontPosture.REGULAR, 24));
+		Text text10 = new Text(" - And a ");
+		text10.setFill(Color.WHITE);
+		text10.setFont(Font.font("Sans-serif", FontWeight.NORMAL, FontPosture.REGULAR, 24));
+		Text text11 = new Text("byte");
+		text11.setFill(Color.WHITE);
+		text11.setFont(Font.font("Sans-serif", FontWeight.BOLD, FontPosture.ITALIC, 28));
+		text11.setUnderline(true);
+		Text text12 = new Text(" is made up of 8 bits, such as '1000 0001'.\n\n");
+		text12.setFill(Color.WHITE);
+		text12.setFont(Font.font("Sans-serif", FontWeight.NORMAL, FontPosture.REGULAR, 24));
+		Text text13 = new Text("In-game, you can view the chart on the right hand side to see how binary numbers are read. Basically, they \nare read "
+				+ "right-to-left, where each position is a power of 2. So the far right digit is 2^0, which is equal to 1. \nThe next spot "
+				+ "is 2^1, which is equal to 2, and so on.\n\n");
+		text13.setFill(Color.WHITE);
+		text13.setFont(Font.font("Sans-serif", FontWeight.NORMAL, FontPosture.REGULAR, 24));
+		Text text14 = new Text("In ");
+		text14.setFill(Color.WHITE);
+		text14.setFont(Font.font("Sans-serif", FontWeight.NORMAL, FontPosture.REGULAR, 24));
+		Text text15 = new Text("Nimble Nibbles");
+		text15.setFill(Color.LAWNGREEN);
+		text15.setFont(Font.font("Sans-serif", FontWeight.BOLD, FontPosture.ITALIC, 28));
+		Text text16 = new Text(" the objective is to turn nibbles (like '0010') into their decimal form (in this case, 2).\nYou will be timed on how fast "
+				+ "you are able to convert 10 nibbles to their decimal equivalent.\nThe shorter your time, the higher you'll be placed on the leaderboards.\n\n");
+		text16.setFill(Color.WHITE);
+		text16.setFont(Font.font("Sans-serif", FontWeight.NORMAL, FontPosture.REGULAR, 24));
+		Text text17 = new Text("Bullet Bytes");
+		text17.setFill(Color.LAWNGREEN);
+		text17.setFont(Font.font("Sans-serif", FontWeight.BOLD, FontPosture.ITALIC, 28));
+		Text text18 = new Text(" is a little harder, where you have 60 seconds to convert as many bytes (like '1001 0011')\ninto their decimal form. The more you convert correctly, the higher"
+				+ "you'll be placed on the leaderboards.\n\n");
+		text18.setFill(Color.WHITE);
+		text18.setFont(Font.font("Sans-serif", FontWeight.NORMAL, FontPosture.REGULAR, 24));
+		Text text19 = new Text("Let's try an example. Convert the binary number '");
+		text19.setFill(Color.WHITE);
+		text19.setFont(Font.font("Sans-serif", FontWeight.NORMAL, FontPosture.REGULAR, 24));
+		Text text20 = new Text("0011");
+		text20.setFill(Color.LAWNGREEN);
+		text20.setFont(Font.font("Sans-serif", FontWeight.NORMAL, FontPosture.REGULAR, 24));
+		Text text21 = new Text("' into it's equivalent decimal form. Click the button to \nsee if you got it correct, and then head back to the main menu and select a game.");
+		text21.setFill(Color.WHITE);
+		text21.setFont(Font.font("Sans-serif", FontWeight.NORMAL, FontPosture.REGULAR, 24));
+		
+		flow.getChildren().addAll(text1, text2, text3, text4, text5, text6, text7, text8, text9, text10, text11, text12, text13, text14, text15, text16, text17, text18, text19, text20, text21);
+		vBox.getChildren().add(flow);
+		
+		GridPane gp = new GridPane();
+		TextField textField = new TextField();
+		textField.requestFocus();
+		textField.setFont(Font.font("Sans-serif", FontWeight.NORMAL, FontPosture.REGULAR, 20));
+		textField.setMaxWidth(100.0);
+		textField.setAlignment(Pos.CENTER);
+		gp.add(textField, 0, 0);
+		GridPane.setMargin(textField, new Insets(20, 20, 20, 20));
+		
+		Button checkButton = new Button("Check answer");
+		checkButton.setFont(Font.font("Sans-serif", FontWeight.BOLD, FontPosture.REGULAR, 20));
+		checkButton.setMinHeight(40.0);
+		checkButton.setMinWidth(100.0);
+		gp.add(checkButton, 1, 0);
+		GridPane.setMargin(checkButton, new Insets(20, 20, 20, 20));
+		
+		checkButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				if(textField.getText().equals("3")) {
+					checkButton.setText("Correct!");
+					checkButton.setStyle("-fx-text-fill: green");
+				} else {
+					checkButton.setText("Incorrect");
+					checkButton.setStyle("-fx-text-fill: red");
+				}
+			}
+		});		
+		
+		gp.setAlignment(Pos.CENTER);
+		vBox.getChildren().add(gp);
+		
+		Button backToMenu = new Button("Back to Menu");
+		backToMenu.setFont(Font.font("Sans-serif", FontWeight.BOLD, FontPosture.REGULAR, 32));
+		backToMenu.setMinHeight(60.0);
+		backToMenu.setMinWidth(200.0);
+		backToMenu.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				vBox.getChildren().clear();
+				layoutView();
+			}
+		});	
+
+		vBox.setAlignment(Pos.CENTER);
+		vBox.getChildren().add(backToMenu);
+		VBox.setMargin(backToMenu, new Insets(20, 20, 20, 20));
+		VBox.setMargin(flow, new Insets(20, 20, 20, 20));
+	}
+	
+	private void layoutNimbleNibbles() {
+		
+	}
 	
 	private void checkIfWin() {
 		
