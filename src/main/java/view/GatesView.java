0b694ff3f4b8f668139eb61c16378f6aa951f8f3
cssetton <cssetton@email.arizona.edu>
2019-11-19 14:16:57 -0700

    changed layout of the menu

--- src/main/java/view/GatesView.java
+++ src/main/java/view/GatesView.java
@@ -9,9 +9,11 @@ import java.util.Observer;
 import javafx.animation.Animation;
 import javafx.animation.Transition;
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
@@ -41,10 +43,10 @@ import model.Wire;
 import model.XORGateGame;
 
 public class GatesView extends MinigameView {
-	private StackPane stackPane;
 	private Image emptyTile;
 	private Image lampOn;
 	private Image lampOff;
+	private Image textBox;
 	private Label text;
 	private GridPane layOut;
 	private ANDGateGame and;
@@ -57,6 +59,9 @@ public class GatesView extends MinigameView {
 	private Button challenge3;
 	private Button tutorial;
 	private Button back;
+	private Background background;
+	private BackgroundFill background_fill;
+	
 	GatesGame curGame;
 	private void  setGatesView(GatesGame game) {
 		curGame=game;
@@ -101,6 +106,8 @@ public class GatesView extends MinigameView {
 		}
 		this.getChildren().add(layOut);
 		this.getChildren().add(back);
+		//layOut.setAlignment(Pos.CENTER);
+		//back.setAlignment(Pos.BOTTOM_RIGHT);
 	}
 	 /**
 	   * This changes and updates the view every game sate change
@@ -122,7 +129,7 @@ public class GatesView extends MinigameView {
 	@Override
 	protected void layoutScene() {
 		BackgroundFill background_fill = new BackgroundFill(Color.rgb(8,20,30),CornerRadii.EMPTY, Insets.EMPTY);
-		Background background = new Background(background_fill); 
+		background = new Background(background_fill); 
 		this.setMinWidth(1200);
 		this.setMinHeight(900);
 		layOut= new GridPane();
@@ -131,10 +138,54 @@ public class GatesView extends MinigameView {
 		xor= new XORGateGame();
 		ch3= new Challenge3GatesGame();
 		text=new Label();
+		String buttonStyle = "-fx-background-color: #c3a38a;-fx-border-width: 0 3 3 0;-fx-background-radius:0px;";
+		String hoverStyle = "-fx-background-color: #816271;-fx-border-width: 0 3 3 0; -fx-background-insets: 0; -fx-background-radius:0px;";
+		//setting up the buttons
 		ORButton=new Button("OR Gate");
+		ORButton.setFont(new Font("Sans-serif", 20));
+		ORButton.setStyle(buttonStyle);
+		ORButton.setMinHeight(50);
+		ORButton.setMinWidth(125);
+		ORButton.setOnMouseEntered(e -> {
+			ORButton.setStyle(hoverStyle);
+		});
+		ORButton.setOnMouseExited(e -> {
+			ORButton.setStyle(buttonStyle);
+		});
+		
 		ANDButton=new Button("AND Gate");
+		ANDButton.setFont(new Font("Sans-serif", 20));
+		ANDButton.setMinHeight(50);
+		ANDButton.setMinWidth(125);
+		ANDButton.setStyle(buttonStyle);
+		ANDButton.setOnMouseEntered(e -> {
+			ANDButton.setStyle(hoverStyle);
+		});
+		ANDButton.setOnMouseExited(e -> {
+			ANDButton.setStyle(buttonStyle);
+		});
+		
 		XORButton=new Button("XOR Gate");
+		XORButton.setFont(new Font("Sans-serif", 20));
+		XORButton.setMinHeight(50);
+		XORButton.setMinWidth(125);
+		XORButton.setStyle(buttonStyle);
+		XORButton.setOnMouseEntered(e -> {
+			XORButton.setStyle(hoverStyle);
+		});
+		XORButton.setOnMouseExited(e -> {
+			XORButton.setStyle(buttonStyle);
+		});
 		challenge3= new Button("Challenge 3");
+		challenge3.setMinHeight(50);
+		challenge3.setMinWidth(125);
+		challenge3.setStyle(buttonStyle);
+		challenge3.setOnMouseEntered(e -> {
+			challenge3.setStyle(hoverStyle);
+		});
+		challenge3.setOnMouseExited(e -> {
+			challenge3.setStyle(buttonStyle);
+		});
 		tutorial= new Button("Tutorial");
 		back = new Button("Back");
 		back.setOnAction( ae -> {
@@ -161,10 +212,12 @@ public class GatesView extends MinigameView {
 			//this.getChildren().clear();
 			setTutorial();
 		});
+		//set up the images
 		try {
 			emptyTile=new Image(new FileInputStream("Image/empty.png"));
 			lampOff=new Image(new FileInputStream("Image/lightbulb.png"));
 			lampOn=new Image(new FileInputStream("Image/lightbulb_lit.png"));
+			textBox= new Image(new FileInputStream("Image/invo-Background.png"));
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -176,19 +229,30 @@ public class GatesView extends MinigameView {
 		
 	}
 	public void setTutorial() {
-		this.getChildren().clear();
-		BackgroundFill background_fill = new BackgroundFill(Color.rgb(8,20,30),CornerRadii.EMPTY, Insets.EMPTY);
-		Background background = new Background(background_fill); 
+		//ObservableList list = stackPane.getChildren();
+		//list.addAll(text,textBox,layOut);
+		
 		
 	}
 	public void setMenu() {
-		VBox vbox= new VBox();
-		vbox.getChildren().add(new Label("Choose your level"));
-		vbox.getChildren().add(ANDButton);
-		vbox.getChildren().add(ORButton);
-		vbox.getChildren().add(XORButton);
-		vbox.getChildren().add(challenge3);
-		this.getChildren().add(vbox);
+		this.setBackground(background);
+		VBox vBox = new VBox();
+		Label l= new Label("Choose your level");
+		l.setFont(new Font("Sans-serif", 40));
+		l.setTextFill(Color.WHITE);
+		vBox.getChildren().add(l);
+		vBox.getChildren().add(ANDButton);
+		vBox.getChildren().add(ORButton);
+		vBox.getChildren().add(XORButton);
+		vBox.getChildren().add(challenge3);
+		vBox.setAlignment(Pos.CENTER);
+		vBox.prefHeight(900);
+		vBox.prefWidth(1200);
+		VBox.setMargin(ANDButton, new Insets(20, 20, 20, 20));
+		VBox.setMargin(ORButton, new Insets(20, 20, 20, 20));
+		VBox.setMargin(XORButton, new Insets(20, 20, 20, 20));
+		VBox.setMargin(challenge3, new Insets(20, 20, 20, 20));
+		this.getChildren().add(vBox);
 		
 	}
 	private void checkState() {
